26e4dc0790146e6dce8c9f377f82fcac7be6debc
pingzing <mcalistern@gmail.com>
2020-12-15 17:31:35 +0200

    Formatting!

--- day_15/src/main.rs
+++ day_15/src/main.rs
@@ -16,20 +16,20 @@ fn get_nth_number(n: u32) -> u32 {
     for (i, value) in INPUT.iter().enumerate() {
         last_spoken_tracker.insert(*value, i);
         prev_spoken = *value;
-    }       
+    }
 
-    for turn in 7..n {     
+    for turn in 7..n {
         let cur;
-        if let Some(last_seen_idx) = last_spoken_tracker.get(&prev_spoken) {                           
-                cur = turn - 1 - *last_seen_idx as u32;
-            } else {                           
-                cur = 0;
-            }        
-        
+        if let Some(last_seen_idx) = last_spoken_tracker.get(&prev_spoken) {
+            cur = turn - 1 - *last_seen_idx as u32;
+        } else {
+            cur = 0;
+        }
+
         // Now that prev_spoken is about to be at i-2, it can safely live in the last_spoken_tracker
         last_spoken_tracker.insert(prev_spoken, turn as usize - 1);
         prev_spoken = cur;
     }
-        
+    
     prev_spoken
 }
